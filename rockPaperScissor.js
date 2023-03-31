2680039888a3921c653a3668f43f1523d53d1278
fatimaahasnain <fatimahasnain987@gmail.com>
2023-03-31 21:51:19 +0500

    added getComputerChoice function

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,1 +1,23 @@
-let num=3
+function getComputerChoice()
+{
+    let num =Math.floor(Math.random() * 4);
+    choice=''
+    if(num==1){
+        choice='rock';
+    }
+    else 
+    if(num==2){
+        choice='paper';
+    }
+    else 
+    if(num==3){
+        choice='scissor';
+    }
+ return choice;
+}
+
+const playerSelection = "rock";
+const computerSelection= getComputerChoice();
+console.log(computerSelection)
+
+
