067fdac6a01b1d605470ff3882108aa0d7f87afd
fatimaahasnain <fatimahasnain987@gmail.com>
2023-03-31 23:04:18 +0500

    mot included loop

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -2,7 +2,7 @@ userChoice= prompt('Enter your weapon: ');
 const playerSelection=userChoice.toLowerCase()
 const computerSelection= getComputerChoice();
 console.log(computerSelection)
-alert(gameGround(playerSelection,computerSelection))
+console.log(gameGround(playerSelection,computerSelection))
 
 function getComputerChoice()
 {
@@ -23,19 +23,18 @@ function getComputerChoice()
 }
 
 function  gameGround(playerSelection,computerSelection){
-    msg=''
+    msg='';
     if((playerSelection=='rock'&&computerSelection=='scissor')||(playerSelection=='scissor'&&computerSelection=='paper')||
     (playerSelection=='paper'&&computerSelection=='rock')){
-        msg='player won!'}
+        msg='PLayer won';}
         else if(playerSelection==computerSelection){
-            msg="It's a tie!"
+            msg='TIE!'
         }
-        else 
-        msg = 'Computer won!'; 
-    
+        
+        else {
+            msg='Computer won!'
+        }
 return msg;
 }
-
-
 
 
